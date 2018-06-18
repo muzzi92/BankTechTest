10facca6147e996a9634e79789721666055f711f
Muzzi Aldean <amiraldean@gmail.com>
2018-06-18 12:29:36 +0100

    Bank function Withdraw debits balance

--- spec/BankSpec.js
+++ spec/BankSpec.js
@@ -1,11 +1,26 @@
-describe("Bank", function(){
+describe('Bank', function(){
   var bank;
   beforeEach(function(){
     bank = new Bank();
   })
 
-  it("Has a balance", function(){
+  it('Has a balance', function(){
     expect(bank.balance).toEqual(0);
+  });
+
+  it('Start with an empty array to store transactions', function(){
+    expect(bank.transactions).toEqual([]);
+  })
+
+  describe('#Withdraw', function(){
+    it('Reduces balance by 10 when parameter is 10', function(){
+      bank.withdraw(10);
+      expect(bank.balance).toEqual(-10);
+    })
+    it('Creates a new instance of Transaction', function(){
+      instance = bank.withdraw();
+      expect(instance instanceof Transaction).toBeTruthy();
+    })
   })
 
 })
--- src/Bank.js
+++ src/Bank.js
@@ -1,3 +1,9 @@
 function Bank(){
   this.balance = 0;
+  this.transactions = [];
 };
+
+Bank.prototype.withdraw = function(amount){
+  this.balance -= amount;
+  return new Transaction('debit', amount);
+}
