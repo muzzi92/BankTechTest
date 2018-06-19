4341789e3c0961c24b2f0fed546edd48f679e1ee
Muzzi Aldean <amiraldean@gmail.com>
2018-06-19 21:29:06 +0100

    Made a transactionClass attribute in Bank

--- src/Bank.js
+++ src/Bank.js
@@ -1,16 +1,17 @@
 function Bank(){
   this.balance = 0;
   this.transactions = new TransactionHistory();
+  this.transactionClass = Transaction;
 }
 
 Bank.prototype.withdraw = function(amount){
   this.balance -= amount;
-  this.transactions.addToLog(new Transaction('debit', amount, this.balance));
+  this.transactions.addToLog(this.transactionClass('debit', amount, this.balance));
 };
 
 Bank.prototype.deposit = function(amount){
   this.balance += amount;
-  this.transactions.addToLog(new Transaction('credit', amount, this.balance));
+  this.transactions.addToLog(this.transactionClass('credit', amount, this.balance));
 };
 
 Bank.prototype.statement = function(){
