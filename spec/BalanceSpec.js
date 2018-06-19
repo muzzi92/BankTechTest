9e5fd670db727ad3badf5871395727b03620509e
Muzzi Aldean <amiraldean@gmail.com>
2018-06-19 12:42:57 +0100

    Created an increase function for Balance class

--- spec/BalanceSpec.js
+++ spec/BalanceSpec.js
@@ -1,6 +1,18 @@
 describe('Balance', function(){
+
+  var balance;
+
+  beforeEach(function(){
+    balance = new Balance();
+  })
+
   it('Starts with a current balance of 0', function(){
-    var balance = new Balance();
     expect(balance.current).toEqual(0);
+  })
+  describe('#Incease', function(){
+    it('Increases balance by 10 when input argument is 10', function(){
+      balance.increase(10);
+      expect(balance.current).toEqual(10);
+    })
   })
 })
--- src/Balance.js
+++ src/Balance.js
@@ -1,3 +1,7 @@
 function Balance(){
   this.current = 0;
 };
+
+Balance.prototype.increase = function(amount) {
+  this.current += amount;
+}
